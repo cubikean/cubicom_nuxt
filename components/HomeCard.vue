1c7e1b155bf5c853a37de4aa2358f69141af9b05
Cubikon <hugochatigny@gmail.com>
2022-09-23 15:08:28 +0200

    init

---
+++ components/HomeCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <img :src="home.images[0]" style="width:200px" alt=""><br />
+        {{ home.title }} <br />
+        {{ home.location.adress }} {{ home.location.city }} {{ home.location.state }}<br />
+        {{ home.guest }} guests<br />
+        {{ home.pricePerNight }} €<br />
+    </div>
+</template>
+
+<script>
+    export default{
+        props:{
+            home:{
+                type: Object,
+                required: true,
+            }
+        }
+    }
+    
+    </script>
+    
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div v-for="home in homes" :key="home.objectID" style="display:flex;justify-content:space-between">
+      <home-card :home="home"/>
+    </div>
+</div>
+</template>
+
+<script>
+import homes from '~/data/homes';
+
+export default{
+  data(){
+    return{
+      homes: homes.slice(0,3)
+    }
+  }
+}
+
+</script>
